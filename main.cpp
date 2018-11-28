3a5a314ff010d3881dd5ceb087e6d200a843d1bb
Doran Kayoumi <dorankayoumi@gmail.com>
2018-11-28 11:09:39 +0100

    Add file header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo 06
+ Fichier     : main.cpp
+ Auteur(s)   : Emmanuel Janssens, Sylvain Laydernier, Doran Kayoumi
+ Date        : 28.11.2018
+
+ But         :
+
+ Remarque(s) :
+
+ Compilateur : g++<6.3.0>
+ -----------------------------------------------------------------------------------
+ */
 #include <iostream>
+
+using namespace std;
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
